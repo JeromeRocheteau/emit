50928d0f36f7d2eb593652a1df5a8fed76c3095d
besselat <besselat@I5LABOINFO5.icam2008.nantes.fr>
2017-03-15 17:57:13 +0100

    maj

--- emit-core/src/main/java/fr/icam/emit/servlets/ImportTest.java
+++ emit-core/src/main/java/fr/icam/emit/servlets/ImportTest.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.PrintWriter;
+
 
 
 import javax.servlet.ServletContext;
@@ -12,68 +12,49 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.github.servlet.jdbc.JdbcUpdateServlet;
+import com.github.servlet.jdbc.JdbcServlet;
 
 
 
-public class ImportTest extends JdbcUpdateServlet<Boolean>{
-private static final long serialVersionUID = 201703141635L;
 
-
+public class ImportTest extends JdbcServlet{
+private static final long serialVersionUID = 201703151635L;	
 	
-	
-
-	
-	@Override
-	protected Boolean doMap(HttpServletRequest request, int count) throws Exception {
-		
-		return count > 0;
-	}
 	
 	@Override
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
 		String file_name = request.getParameter("file_name");
-		this.doPrint(this.read_file(response,file_name), response);
+		this.doPrint(this.read_file(file_name), response);
 		
 		
 	}
 	
 	
-	public String read_file(HttpServletResponse response,String file_name){
+	public String read_file(String file_name){
 		String s;
 		s= "";
+		String file_content = "";
 		 
 		ServletContext context = getServletContext();
 		InputStream is = context.getResourceAsStream("WEB-INF/classes/"+file_name);
 		if (is != null) {
 			InputStreamReader isr = new InputStreamReader(is);
 			BufferedReader reader = new BufferedReader(isr);
-			PrintWriter writer = null;
-			try {
-				writer = response.getWriter();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			
-			
-			//hi
-			// We read the file line by line and later will be displayed on the 
-			// browser page.
-			//
+						
 			try {
 				while ((s = reader.readLine()) != null) {
-					writer.println(s);
+					//writer.println(s);
+					file_content = s+ file_content;
 				}
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}else {
-			s = context.toString();
+			file_content = "le fichier n'existe pas";
 		}
 		
 	
-		return s;
+		return file_content;
 	};
 }
