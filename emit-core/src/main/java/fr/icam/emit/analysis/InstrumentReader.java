af0e201f061bdd6e2f09358c3cb27a3e42b33567
BastSamson <bastien.samson@2017.icam.fr>
2017-04-12 11:53:05 +0200

    modifs reader

--- emit-core/src/main/java/fr/icam/emit/analysis/InstrumentReader.java
+++ emit-core/src/main/java/fr/icam/emit/analysis/InstrumentReader.java
@@ -6,22 +6,35 @@ import java.util.List;
 import java.util.Scanner;
 
 public class InstrumentReader {
-	
-	public List<List<String>> Read(String input) {
 
-		List<List<String>> lines = new ArrayList<List<String>>();
-		Scanner inputStream;
-	    inputStream = new Scanner(input);
-	    
-		while(inputStream.hasNext()){
-			
-            String line= inputStream.next();
-            String[] values = line.split(" ");
-            lines.add(Arrays.asList(values));
-        }
-		
-		return lines;
+	public InstrumentReader() {
 	}
 	
-        
+	List<List<String>> lines = new ArrayList<List<String>>();
+
+	public List<List<String>> Read(Scanner inputStream) {
+
+		while (inputStream.hasNext()) {
+
+			String line = inputStream.next();
+			String[] values = line.split(" ");
+			lines.add(Arrays.asList(values));
+		}
+
+		return lines;
+	}
+
+	public void Afficher() {
+
+		int lineNo = 1;
+		for (List<String> line : lines) {
+			int columnNo = 1;
+			for (String value : line) {
+				System.out.println("Line " + lineNo + " Column " + columnNo + ": " + value);
+				columnNo++;
+			}
+			lineNo++;
+		}
+
+	}
 }
