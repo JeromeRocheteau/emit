76750c2e309e504840eee13b27c6eaf1c1704a44
Jérôme Rocheteau <jerome.rocheteau@free.fr>
2017-09-07 09:26:01 +0200

    monitoring use case done (chart inf/sup bounds to improve)

--- emit-monitor/emit-monitor-services/src/main/java/fr/icam/emit/listeners/MqttListener.java
+++ emit-monitor/emit-monitor-services/src/main/java/fr/icam/emit/listeners/MqttListener.java
@@ -14,47 +14,65 @@ import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
 
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.MongoIterable;
 
 public class MqttListener extends MqttClient implements MqttCallback {
 
 	private static Map<Long, MqttListener> instances = new HashMap<Long, MqttListener>(124);
 
 	public static void start(Long id, String uri, String uuid, String topic, MongoDatabase database) throws MqttException {
+		System.out.println("[MongoDB] starting " + id + " to '" + topic + "' in " + uuid);
 		MqttListener listener = instances.get(id);
 		if (listener == null) {
 			listener = new MqttListener(id, uri, uuid, topic, database);
 			instances.put(id, listener);
 		}
 		listener.subscribe(topic);
+		System.out.println("[MongoDB] started " + id + " to '" + topic + "' in " + uuid);
 	}
 	
 	public static void stop(Long id) throws MqttException {
+		System.out.println("[MongoDB] stopping " + id);
 		MqttListener listener = instances.get(id);
 		if (listener == null) {
 			throw new MqttException(new NullPointerException("No MQTT Client found for feature id = '" + id.toString() +"'"));
 		}
 		listener.disconnect();
 		instances.remove(id);
+		System.out.println("[MongoDB] stopped " + id);
 	}
 	
 	private MongoCollection<Document> collection;
 	
 	private MqttListener(Long id, String uri, String uuid, String topic, MongoDatabase db) throws MqttException {
 		super(uri, uuid, new MemoryPersistence());
-		this.collection = db.getCollection(this.getClientId());
+		System.out.println("[MongoDB] creating " + uuid);
+		db.createCollection(uuid);
+		this.collection = db.getCollection(uuid);
 		MqttConnectOptions options = new MqttConnectOptions();
 		options.setCleanSession(true);
 		this.connect(options);
 		this.setCallback(this);
+		System.out.println("[MongoDB] created " + uuid);
+		System.out.println("[MongoDB] existing " + this.doExists(uuid, db));
 	}
 
+	private boolean doExists(final String uuid, MongoDatabase db) {
+	     MongoIterable<String> names = db.listCollectionNames();
+	    for (final String name : names) {
+	        if (name.equalsIgnoreCase(uuid)) {
+	            return true;
+	        }
+	    }
+	    return false;
+	}
+	
 	public void connectionLost(Throwable throwable) {
-		// TODO Auto-generated method stub
-		
+		System.out.println("[MongoDB] failure: ");
+		throwable.printStackTrace();
 	}
 
 	public void deliveryComplete(IMqttDeliveryToken token) {
-		// TODO Auto-generated method stub
 		
 	}
 
@@ -65,6 +83,7 @@ public class MqttListener extends MqttClient implements MqttCallback {
 		document.append("value", value);
 		document.append("time", System.currentTimeMillis()); /* FIXME */
 		collection.insertOne(document);
+		System.out.println("[MongoDB] insert: " + document.toJson());
 	}
 
 }
